a728a934d8797a10722d317ad2565cc18d8853f4
Ryan Harrington <harrington.ryana@gmail.com>
2019-05-10 17:33:57 -0600

    Added a main method with comments.

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -2,7 +2,11 @@
 public class MakeChangeApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//		Prompt user for value
+		
+//		prompt user for amt tendered
+		
+//		
 
 	}
 
