81e79cc46a2c2c818399e5ecdbbcf7877fce0dfb
Ryan Harrington <harrington.ryana@gmail.com>
2019-05-11 13:20:02 -0600

    MVP user story 1 complete

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -1,13 +1,18 @@
+import java.util.Scanner;
 
 public class MakeChangeApp {
 
 	public static void main(String[] args) {
-//		Prompt user for value
-		
-//		prompt user for amt tendered
-		
-//		
+		double price = askPrice();
+		System.out.println(price);
 
+	}
+	
+	public static double askPrice() {
+		Scanner kb = new Scanner(System.in);
+		System.out.println("How much does this item cost? ");
+		double price = kb.nextDouble();
+		return price;
 	}
 
 }
