4a0d06b992480181d1ea42569a9217f212d49b1e
Ryan Harrington <harrington.ryana@gmail.com>
2019-05-11 13:47:35 -0600

    Inform user that payment is correct/incorrect

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -7,21 +7,40 @@ public class MakeChangeApp {
 		double price = askPrice(kb);
 		System.out.println(price);
 		System.out.println();
-		double amounPaid = amountTendered(kb);
-		System.out.println(amounPaid);
+		double payment = amountTendered(kb);
+		System.out.println(payment);
+		verifyPayment(price, payment);
 
 	}
-	
+
 	public static double askPrice(Scanner kb) {
-		System.out.println("How much does this item cost? ");
+		System.out.println("How much does this item cost? (x.xx)");
 		double price = kb.nextDouble();
 		return price;
 	}
-	
+
 	public static double amountTendered(Scanner kb) {
 		System.out.print("Amount paid: ");
 		double amountTendered = kb.nextDouble();
 		return amountTendered;
+	}
+
+	public static void verifyPayment(double price, double payment) {
+		int scaledPrice = makeInt(price);
+		int scaledPayment = makeInt(payment);
+		if (scaledPrice > scaledPayment) {
+			System.out.println("Insufficient payment. You owe: ");
+			
+		}	else if (scaledPrice < scaledPayment) {
+			System.out.println("Thank you. Pay customer: ");
+			
+		}	else {
+			System.out.println("Thank you. Come again.");				
+		}
+	}
+	
+	public static int makeInt(double amount) {
+		return (int)(amount * 100);
 	}
 
 }
