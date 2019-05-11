85d42030cf080c621e7e9e38508e297e3a2d07f9
Ryan Harrington <harrington.ryana@gmail.com>
2019-05-11 14:32:58 -0600

    Calculate change due/owed

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -28,11 +28,14 @@ public class MakeChangeApp {
 	public static void verifyPayment(double price, double payment) {
 		int scaledPrice = makeInt(price);
 		int scaledPayment = makeInt(payment);
+		System.out.println(scaledPrice);
+		System.out.println(scaledPayment);
 		if (scaledPrice > scaledPayment) {
-			System.out.println("Insufficient payment. You owe: ");
-			
+			System.out.print("Insufficient payment. Customer owes: Cents:");
+			remainderInCents(scaledPrice, scaledPayment);
 		}	else if (scaledPrice < scaledPayment) {
-			System.out.println("Thank you. Pay customer: ");
+			System.out.print("Thank you. Pay customer: Cents: ");
+			remainderInCents(scaledPrice, scaledPayment);
 			
 		}	else {
 			System.out.println("Thank you. Come again.");				
@@ -41,6 +44,17 @@ public class MakeChangeApp {
 	
 	public static int makeInt(double amount) {
 		return (int)(amount * 100);
+	}
+	
+	public static double remainderInCents(int price, int payment) {
+		double newPrice = price;
+		double newPayment = payment;
+		double change = newPrice % newPayment;
+		if (price < payment) {
+			change = newPayment - newPrice;
+		}
+		System.out.print(change);
+		return change;
 	}
 
 }
