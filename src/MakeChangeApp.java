bcaed157599b128202199145e4a0dedc5d6d2120
Ryan Harrington <harrington.ryana@gmail.com>
2019-05-11 13:27:47 -0600

    MVP user story 2 complete

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -3,16 +3,25 @@ import java.util.Scanner;
 public class MakeChangeApp {
 
 	public static void main(String[] args) {
-		double price = askPrice();
+		Scanner kb = new Scanner(System.in);
+		double price = askPrice(kb);
 		System.out.println(price);
+		System.out.println();
+		double amounPaid = amountTendered(kb);
+		System.out.println(amounPaid);
 
 	}
 	
-	public static double askPrice() {
-		Scanner kb = new Scanner(System.in);
+	public static double askPrice(Scanner kb) {
 		System.out.println("How much does this item cost? ");
 		double price = kb.nextDouble();
 		return price;
+	}
+	
+	public static double amountTendered(Scanner kb) {
+		System.out.print("Amount paid: ");
+		double amountTendered = kb.nextDouble();
+		return amountTendered;
 	}
 
 }
